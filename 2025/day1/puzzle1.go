4bd3b79fc1ec4940527bd378038aae179d627f55
Manuel3115 <manuel-etudes@outlook.com>
2025-12-02 19:20:22 -0500

    First puzzle for day1 2025

---
+++ 2025/day1/puzzle1.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+)
+
+func main() {
+	file, err := os.Open("puzzle1_input.txt")
+
+	if err != nil {
+		log.Fatalf("failed to open file: %v", err)
+	}
+
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	total := 50
+	zeroCounts := 0
+
+	for scanner.Scan() {
+		line := scanner.Text()
+		direction := line[0:1]
+		num, err := strconv.Atoi(line[1:])
+
+		if err != nil {
+			fmt.Printf("The line number in the input file is wrong (%s).\n", line[1:])
+			return
+		}
+
+		switch direction {
+		case "R":
+			total = (total + num) % 100
+		case "L":
+			total = (total - num) % 100
+		default:
+			fmt.Printf("The line direction format in the input file is wrong (%s).\n", direction)
+			return
+		}
+
+		if total == 0 {
+			zeroCounts += 1
+		}
+	}
+
+    fmt.Printf("The dial reached zero %d times.\n", zeroCounts)
+}
---
+++ 2025/go.mod
@@ -0,0 +1,3 @@
+module AdventOfCode/2025
+
+go 1.25.5
